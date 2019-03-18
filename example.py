d270af1081641213793958c39f57cabf349146ba
alexafoo <kexin9797@gmail.com>
2019-03-18 16:22:48 -0400

    test2

--- example.py
+++ example.py
@@ -1,18 +1,21 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 
-# Create a new chat bot named Charlie
-chatbot = ChatBot('Charlie')
 
-trainer = ListTrainer(chatbot)
+bot = ChatBot(
+    'Unit Converter',
+    logic_adapters=[
+        'chatterbot.logic.UnitConversion',
+    ]
+)
 
-trainer.train([
-    "Hi, can I help you?",
-    "Sure, I'd like to book a flight to Iceland.",
-    "Your flight has been booked."
-])
+questions = [
+    'How many meters are in a kilometer?',
+    'How many meters are in one inch?',
+    '0 celsius to fahrenheit',
+    'one hour is how many minutes ?'
+]
 
-# Get a response to the input text 'I would like to book a flight.'
-response = chatbot.get_response('I would like to book a flight.')
-
-print(response)
+# Prints the convertion given the specific question
+for question in questions:
+    response = bot.get_response(question)
+    print(question + ' -  Response: ' + response.text)
