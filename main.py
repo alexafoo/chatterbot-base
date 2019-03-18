bfcdc82217d6b5f09be82fe84fc9e74bedde53e7
alexafoo <kexin9797@gmail.com>
2019-03-18 18:03:08 -0400

    test form + socketIO

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_socketio import SocketIO
 app = Flask(__name__)
 
 #default page
 @app.route("/")
 def home():
     return render_template("index.html");
+
+#submit chat
+@app.route('/submit', methods = ['POST'])
+def submit():
+    text_data = request.form['text']
+    print("The text is '" + text_data + "'")
+    return redirect('/');
 
 if __name__ == "__main__":
     app.run(debug=True)
