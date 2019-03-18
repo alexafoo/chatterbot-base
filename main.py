c135664f457468b1e635aa438d7441c56dbf2e66
alexafoo <kexin9797@gmail.com>
2019-03-18 18:31:19 -0400

    socketIO intergration + form error

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chatbotbeepboop'
+socketio = SocketIO(app)
 
 #default page
 @app.route("/")
 def home():
     return render_template("index.html");
+
+#msg received
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 #submit chat
 @app.route('/submit', methods = ['POST'])
