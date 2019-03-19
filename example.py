fe420793c08b3bdd3f7c4b44c3fc67ae29cf7490
MioMiosama <ccfxnx30704@126.com>
2019-03-18 20:35:00 -0400

    Version 2

--- example.py
+++ example.py
@@ -26,11 +26,14 @@ c = conn.cursor()
 #c.execute('INSERT INTO cases1 VALUES("Hola","Hola back")')
 #c.execute('INSERT INTO cases VALUES("What is your name","IDK")')
 #c.execute('INSERT INTO cases VALUES("exit","See ya back")')
-var1=input()
-var2=input()
-
-c.execute("INSERT INTO cases1 VALUES(%?,?)", (var1, var2))
-conn.commit()
+print("Please enter your question, type 'y' or 'n' to continue")
+if input()!= 'n':
+    print("Please enter your question.")
+    var1=input()
+    print("Please enter your answer.")
+    var2=input()
+    c.execute("INSERT INTO cases1 VALUES(?,?)", (var1, var2))
+    conn.commit()
 
 print('Type something to begin...')
 botName = "I don't have a name yet"
@@ -39,22 +42,23 @@ while True:
     try:
         user_input = input()
 
-        if user_input == var1:
-            bot_response = "see ya!"
-            c.execute("select answers from cases1 where questions = ?",var1)
-       #     sql_parameterized_query = """select answers from cases where questions = %s """
-       #     c.execute(sql_parameterized_query, user_input)
-            results = c.fetchall()
-            print(results)
-            print  (bot_response)
-            break
+
+        bot_response = "see ya!"
+        c.execute("select answers from cases1 where questions='%s'"%user_input)
+   #     sql_parameterized_query = """select answers from cases where questions = %s """
+   #     c.execute(sql_parameterized_query, user_input)
+        results = c.fetchone()
+        almost=(str(results))
+        print(almost[2:-3])
+        print  (bot_response)
+
      #   elif user_input == c.execute("select * from cases where questions=: user_input()", {botName: "select * from cases where answers"}):
 
     #        print(botName)
-        elif user_input == "How may I call you":
-            print(botName)
-        else:
-            print("I don't understand")
+        #elif user_input == "How may I call you":
+         #   print(botName)
+        #else:
+        #    print("I don't understand")
        #     bot_response = bot.get_response(user_input)
 
        # print(bot_response)
--- main.py
+++ main.py
@@ -40,6 +40,12 @@ if __name__ == "__main__":
 
 
 
+# Uncomment the following lines to enable verbose logging
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
+# Create a new instance of a ChatBot
+
 bot = ChatBot(
     'Terminal',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
@@ -59,11 +65,14 @@ c = conn.cursor()
 #c.execute('INSERT INTO cases1 VALUES("Hola","Hola back")')
 #c.execute('INSERT INTO cases VALUES("What is your name","IDK")')
 #c.execute('INSERT INTO cases VALUES("exit","See ya back")')
-var1=input()
-var2=input()
-
-c.execute("INSERT INTO cases1 VALUES(?,?)", (var1, var2))
-conn.commit()
+print("Please enter your question, type 'y' or 'n' to continue")
+if input()!= 'n':
+    print("Please enter your question.")
+    var1=input()
+    print("Please enter your answer.")
+    var2=input()
+    c.execute("INSERT INTO cases1 VALUES(?,?)", (var1, var2))
+    conn.commit()
 
 print('Type something to begin...')
 botName = "I don't have a name yet"
@@ -72,28 +81,27 @@ while True:
     try:
         user_input = input()
 
-        if user_input == var1:
-            bot_response = "see ya!"
-            c.execute("select answers from cases1 where questions = ?",var1)
-       #     sql_parameterized_query = """select answers from cases where questions = %s """
-       #     c.execute(sql_parameterized_query, user_input)
-            results = c.fetchall()
-            print(results)
-            print  (bot_response)
-            break
+
+        bot_response = "see ya!"
+        c.execute("select answers from cases1 where questions='%s'"%user_input)
+   #     sql_parameterized_query = """select answers from cases where questions = %s """
+   #     c.execute(sql_parameterized_query, user_input)
+        results = c.fetchone()
+        almost=(str(results))
+        print(almost[2:-3])
+        print  (bot_response)
+
      #   elif user_input == c.execute("select * from cases where questions=: user_input()", {botName: "select * from cases where answers"}):
 
     #        print(botName)
-        elif user_input == "How may I call you":
-            print(botName)
-        else:
-            print("I don't understand")
+        #elif user_input == "How may I call you":
+         #   print(botName)
+        #else:
+        #    print("I don't understand")
        #     bot_response = bot.get_response(user_input)
 
        # print(bot_response)
     # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit,):
         break
-
-
 
