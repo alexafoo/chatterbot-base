99b7291bd3f7e9140977b4fa834c2da65943cbdd
MioMiosama <ccfxnx30704@126.com>
2019-03-29 02:15:38 -0400

    fixed for alexa

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sqlite3
 from chatterbot import ChatBot
 #
 
-
 bot = ChatBot(
     'Terminal',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
@@ -17,11 +16,7 @@ bot = ChatBot(
 )
 
 conn = sqlite3.connect('ddh.db', check_same_thread=False)
-
-#conn = sqlite3.connect('ddh.db')
 c = conn.cursor()
-
-
 
 def access_db(question):
     print(question)
@@ -34,7 +29,6 @@ def access_db(question):
     #print(res1)
     #print("tt")
     return res1
-
 
 
 app = Flask(__name__)
@@ -67,90 +61,20 @@ def submit():
     print("The text is '" + text_data + "'")
     return redirect('/');
 
-if __name__ == "__main__":
-    socketio.run(app)
-    #app.run(debug=True)
-
-
-
 # Uncomment the following lines to enable verbose logging
 # import logging
 # logging.basicConfig(level=logging.INFO)
 
 # Create a new instance of a ChatBot
 
-
-
-
-
-
-#if init == 0:
-#c.execute("CREATE TABLE cases1 (questions str, answers str)")
-#else :
-#c.execute('INSERT INTO cases1 VALUES("Hola","Hola back")')
-#c.execute('INSERT INTO cases VALUES("What is your name","IDK")')
-#c.execute('INSERT INTO cases VALUES("exit","See ya back")')
-
-#commented all
-'''
-print("Please enter your question, type 'y' or 'n' to continue")
-if input()!= 'n':
-    print("Please enter your question.")
-    var1=input()
-    print("Please enter your answer.")
-    var2=input()
-    c.execute("INSERT INTO cases1 VALUES(?,?)", (var1, var2))
-    conn.commit()
-
-print('Type something to begin...')
-botName = "I don't have a name yet"
-# The following loop will execute each time the user enters input
-while True:
-    try:
-        user_input = input()
-
-
-        bot_response = "see ya!"
-        c.execute("select answers from cases1 where questions='%s'"%user_input)
-   #     sql_parameterized_query = """select answers from cases where questions = %s """
-   #     c.execute(sql_parameterized_query, user_input)
-        results = c.fetchone()
-        almost=(str(results))
-        print(almost[2:-3])
-        print (bot_response)
-
-     #   elif user_input == c.execute("select * from cases where questions=: user_input()", {botName: "select * from cases where answers"}):
-
-    #        print(botName)
-        #elif user_input == "How may I call you":
-         #   print(botName)
-        #else:
-        #    print("I don't understand")
-       #     bot_response = bot.get_response(user_input)
-
-       # print(bot_response)
-    # Press ctrl-c or ctrl-d on the keyboard to exit
-
-    except (KeyboardInterrupt, EOFError, SystemExit,):
-        break
-
-'''
-
-
-
 def access_db(question):
     print(question)
     c.execute("select answers from cases1 where questions='%s'" %question)
-    #c.execute("select * from cases1")
     res = c.fetchall()
     print(res)
     res1=(str(res))
     res1 = res1[3:-4]
-    #print(res1)
-    #print("tt")
     return res1
 
-
-#quest="how are you"
-#quest="mmm"
-#print(access_db(quest))
+if __name__ == "__main__":
+    socketio.run(app)
